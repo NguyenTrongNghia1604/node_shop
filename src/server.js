3c9b2066c6a2652273af0b68be29ff2974878efd
NguyenTrongNghia1604 <nghiavip1010@gmail.com>
2024-10-11 17:12:42 +0700

    update

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const port = process.env.PORT || 8081;
 //
 import bodyParser from 'body-parser';
 //
-import session from 'express-session';
+//import session from 'express-session';
 import Redis from 'ioredis';
-import RedisStore from 'connect-redis';
+//import RedisStore from 'connect-redis';
 //
 import cors from 'cors';
 import configCors from './config/cors';
@@ -21,6 +21,9 @@ export const redisClient = new Redis({
     },
 });
 
+console.log('REDIS_HOST', process.env.REDIS_HOST);
+console.log('REDIS_PORT', process.env.REDIS_PORT);
+console.log('PASS', process.env.PASS);
 const app = express();
 
 redisClient.on('connect', () => {
@@ -30,31 +33,31 @@ redisClient.on('connect', () => {
 redisClient.on('error', (err) => {
     console.error('Redis error:', err);
 });
-app.use(
-    session({
-        secret: 'keyboard cat',
-        store: new RedisStore({ client: redisClient }),
-        resave: false, // có nghĩa là đặt lại session cho mỗi yêu cầu, Giả sử gằng cookie hết hạn sau 10p thì nó sẻ đặt thêm 10p nữa cho mỗi lần request sau.
-        saveUninitialized: true, // có nghĩa là bất kì có cookie session hay không mỗi khi cookie và session yêu cầu thì nó được đánh dấu bởi connect.sid theo yêu cầu mặt định
-        //saveUninitialized: false, // Chỉ lưu session khi có dữ liệu
-        cookie: {
-            secure: true, // nếu nó là true thì nó chạy trong https, còn false thì nó chạy trong http
-            httpOnly: true,
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 ngày
-        },
-        // log when the session is saved
-        save: (req, res, next) => {
-            req.session.save((err) => {
-                if (err) {
-                    console.error('Lỗi khi lưu session:', err);
-                } else {
-                    console.log('Session được lưu thành công vào Redis:', req.session);
-                }
-                next();
-            });
-        },
-    }),
-);
+// app.use(
+//     session({
+//         secret: 'keyboard cat',
+//         store: new RedisStore({ client: redisClient }),
+//         resave: false, // có nghĩa là đặt lại session cho mỗi yêu cầu, Giả sử gằng cookie hết hạn sau 10p thì nó sẻ đặt thêm 10p nữa cho mỗi lần request sau.
+//         saveUninitialized: true, // có nghĩa là bất kì có cookie session hay không mỗi khi cookie và session yêu cầu thì nó được đánh dấu bởi connect.sid theo yêu cầu mặt định
+//         //saveUninitialized: false, // Chỉ lưu session khi có dữ liệu
+//         cookie: {
+//             secure: false, // nếu nó là true thì nó chạy trong https, còn false thì nó chạy trong http
+//             httpOnly: true,
+//             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 ngày
+//         },
+//         // log when the session is saved
+//         save: (req, res, next) => {
+//             req.session.save((err) => {
+//                 if (err) {
+//                     console.error('Lỗi khi lưu session:', err);
+//                 } else {
+//                     console.log('Session được lưu thành công vào Redis:', req.session);
+//                 }
+//                 next();
+//             });
+//         },
+//     }),
+// );
 // thêm đoạn này mới định dạng được request api
 // Cấu hình body parser
 // Cấu hình CORS cho tất cả các route
