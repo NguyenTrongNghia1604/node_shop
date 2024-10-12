1b45e944fb9c46916aa961a3a12f1aa37234b7ec
NguyenTrongNghia1604 <nghiavip1010@gmail.com>
2024-10-12 14:36:36 +0700

    up

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,9 @@
 const { Sequelize } = require('sequelize');
-
+require('dotenv').config(); // Load biến môi trường từ file
 // Khởi tạo kết nối Sequelize
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST, // Địa chỉ host của cơ sở dữ liệu
     dialect: 'mysql', // Loại cơ sở dữ liệu, ví dụ: 'mysql', 'postgres', 'sqlite', 'mssql'
-    port: process.env.DB_PORT || 3306,
 });
 
 // Kiểm tra kết nối
